8add27b75785c8f2dc87de377a1db99dab7645c0
Luc Bougé <luc.bouge@irisa.fr>
2020-05-19 17:20:09 +0200

    Starting V2

---
+++ counter.py
@@ -0,0 +1,16 @@
+from .print import eprint
+
+
+class Counter():
+    def __init__(self):
+        self.counter_dict = dict()
+
+    def increment(self, tag):
+        if tag not in self.counter_dict:
+            self.counter_dict[tag] = 0
+        self.counter_dict[tag] += 1
+
+    def eprint(self):
+        eprint('_'*20)
+        for (tag, nb) in sorted(self.counter_dict.items()):
+            eprint(tag + ': ', nb)
